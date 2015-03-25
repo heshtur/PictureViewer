a5783c3ce23c00b09efc979c7b7e85564530577f
heshtur <chrisga1990@gmail.com>
2015-03-25 10:42:49 -0700

    Finished Tut1
    Working picture viewer completed

--- Form1.cs
+++ Form1.cs
@@ -18,27 +18,32 @@ namespace PictureViewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
-
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.Load(openFileDialog1.FileName);
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void backgroundButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
